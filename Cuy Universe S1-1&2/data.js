a9ba212de66d74d6f17197434625f98bb3008c58
amru <117263639+amruTzy@users.noreply.github.com>
2024-07-16 11:43:59 +0700

    Add files via upload

---
+++ Cuy Universe S1-1&2/data.js
@@ -0,0 +1,27 @@
+const nama = "Muhamad Tegar Amru";
+let usia = 10;
+
+let biodata = document.getElementById("biodata");
+
+function generateBioData() {
+  let generasi;
+
+  if (usia > 10 && usia < 18) {
+    generasi = "generasi remaja";
+  } else if (usia > 18 && usia < 30) {
+    generasi = "generasi dewasa";
+  } else if (usia > 30) {
+    generasi = "generasi tua";
+  } else if (usia > 2 && usia < 10) {
+    generasi = "generasi anak";
+  } else {
+    generasi = "generasi bayi";
+  }
+
+  return (biodata.innerHTML = generasi);
+}
+
+console.log(nama);
+console.log(usia);
+
+generateBioData();
